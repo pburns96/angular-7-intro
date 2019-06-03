6b8d2f1fd2dfdbf2750254acaddc36040ae721d4
pburns96 <pburns96@live.com>
2019-06-03 09:57:06 -0400

    Finished the "Managing Data" section of the angular tutorial

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,10 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
+  
+  notifyMe() {
+    window.alert('You will be notified when this product goes on sale!');
+  }
 }
 
 
